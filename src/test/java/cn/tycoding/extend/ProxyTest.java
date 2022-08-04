77551f103ede8200cb310ed3e001bdea17f21350
tycoding <tycoding@sina.com>
2022-08-04 17:56:26 +0800

    增加动态代理测试类

---
+++ src/test/java/cn/tycoding/extend/ProxyTest.java
@@ -0,0 +1,95 @@
+package cn.tycoding.extend;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Java代理设计模式测试。
+ * Java的代理技术主要分为三类：1.JDK静态代理 2.JDK动态代理 3.Cglib动态代理
+ * <p>
+ * 代理模式的逻辑：接口 -> 接口实现类（被代理类） -> 代理类（无论是静态还是动态都是要生成被代理类的引用）
+ *
+ * @author tycoding
+ * @since 2022/8/4
+ */
+public class ProxyTest {
+
+    /**
+     * 测试JDK静态代理
+     * JDK静态代理技术。逻辑：
+     * 1.定义接口
+     * 2.定义接口实现类（被代理类）
+     * 3.定义代理类实现接口（包含被代理类的引用），调用被代理类的处理逻辑
+     */
+    @Test
+    public void t1() {
+        JdkStaticProxy proxy = new JdkStaticProxy();
+        proxy.say();
+    }
+
+    /**
+     * 测试JDK动态代理
+     */
+    @Test
+    public void t2() {
+        ProxyTestInfImpl impl = new ProxyTestInfImpl();
+        ProxyTestHandler handler = new ProxyTestHandler();
+        Object instance = handler.getProxyInstance(impl);
+        System.out.println(instance);
+        ProxyTestInf inf = (ProxyTestInf) instance;
+        inf.say();
+    }
+}
+
+/**
+ * JDK动态代理类。核心：通过反射技术
+ */
+class ProxyTestHandler implements InvocationHandler {
+
+    // 被代理类对象
+    Object obj;
+
+    public Object getProxyInstance(Object obj) {
+        this.obj = obj;
+        return Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return method.invoke(obj, args);
+    }
+}
+
+
+/**
+ * JDK静态代理类。核心：实现代理接口，包含一个被代理类的对象引用，调用被代理类的处理逻辑
+ */
+class JdkStaticProxy implements ProxyTestInf {
+    private ProxyTestInf inf = new ProxyTestInfImpl();
+
+    @Override
+    public void say() {
+        inf.say();
+    }
+}
+
+/**
+ * 接口
+ */
+interface ProxyTestInf {
+    void say();
+}
+
+/**
+ * 被代理的类
+ */
+class ProxyTestInfImpl implements ProxyTestInf {
+
+    @Override
+    public void say() {
+        System.out.println("this is ProxyTestInfImpl");
+    }
+}
